42a0bc25caa7d41ef8abb47785e4d1d66765a8c7
yu-oonishi <yu-oonishi@users.noreply.github.com>
2016-09-09 17:02:44 +0900

    Improve sample view

--- src/main/java/test/resources/SampleResource.java
+++ src/main/java/test/resources/SampleResource.java
@@ -1,5 +1,15 @@
 package test.resources;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,12 +17,26 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class SampleResource {
-    
+
+    /**
+     * リクエストを処理するサンプル
+     * 
+     * @return htmlコンテンツ
+     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String showText() {
-        
-        return "Hello Dropwizard!";
+    @Produces(MediaType.TEXT_HTML)
+    public String showHellowWorld() {
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("index.html").toURI())),
+                    StandardCharsets.UTF_8);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (URISyntaxException urie) {
+            urie.printStackTrace();
+        }
+
+        return content;
     }
 
 }
